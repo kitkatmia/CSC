0f7e9f6573e1848ad17059594fddc840d27a13b9
kitkatmia <alinka.markova@gmail.com>
2021-12-23 16:46:59 -0800

    Added arrow function

--- mvhs.io_project/basics_of_javascript/functions.js
+++ mvhs.io_project/basics_of_javascript/functions.js
@@ -5,7 +5,10 @@ function addition(a, b) { // Notice: you call function, not def
 console.log(addition(5, 6));
 
 // Anonymous Function (think lambda)
-var greet = function(name, time_of_day) {
+const greet = function(name, time_of_day) {
     console.log(`Good ${time_of_day}, ${name}`);
 }
-greet('Bob', 'morning')
+greet('Bob', 'morning');
+
+let add = (a, b) => console.log(a + b);   
+add(1, 2)
