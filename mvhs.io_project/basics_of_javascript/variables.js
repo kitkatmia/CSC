3bf902191f56a704e1bdfdbbfb1c318577015164
kitkatmia <alinka.markova@gmail.com>
2021-12-31 17:03:00 -0800

    Removed moved files

--- mvhs.io_project/basics_of_javascript/variables.js
+++
@@ -1,8 +0,0 @@
-var num = 4; //JavaScript is a "duck-typed," mean9ng every variable is defined using the var keyword, and can contain all types of variables
-var string = "This is a string";
-var bool = false;
-var nullVariable = null; // assigning the value null to a variable acts similalrly to None in python
-var undefined; // This variable would simply be undefined
-
-
-console.log(num);
