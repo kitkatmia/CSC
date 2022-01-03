0a114670b07399fcaac8960b9b0a2379af5d0706
kitkatmia <alinka.markova@gmail.com>
2022-01-03 09:09:09 -0800

    Corrected definitions of local variables

--- mvhs.io_project/basics_of_javascript/free_code_camp_tutorial/scoping.js
+++ mvhs.io_project/basics_of_javascript/free_code_camp_tutorial/scoping.js
@@ -1,1 +1,27 @@
 // This kind of overlaps with a bunch of other stuff, but it's special (and [for now] confusing) enough to get it's own file.
+var global = 10; // this is a global variable that can be refrenced anywhere within the code (includingi in function)
+
+function func1() {
+    // vvvvvvvvv IMPORTANT vvvvvvvvvvv
+    // Since oopGlobal DOES NOT have the word "var" in front of it, it is automatically scoped as a GLOBAL variable
+    // ^^^^^^ IMPORTANT ^^^^^^^^^^^
+    oopsGlobal = 1;
+    var localVar = 'Yo, Wassup'; // this, on the other hand, is a local variable that is only visible within the function
+}
+
+function fun2() {
+    var output = "";
+    if (typeof global != 'undefined') { // typeof just checks the type of the var/object/etc
+        output += "Global var: " + global + '\n';
+    }
+    if (typeof oopsGlobal != 'undefined') {
+        output += 'OopsGlobal var: ' + oopsGlobal + '\n';
+    }
+    if (typeof localVar != 'undefined') {
+        output += 'localVar var: ' + localVar + '\n'; // typeof localVar == undefined but if you try to print it, you'll recieve an error (because it's not defined)
+    }
+    console.log(output);
+}
+
+func1();
+fun2();
