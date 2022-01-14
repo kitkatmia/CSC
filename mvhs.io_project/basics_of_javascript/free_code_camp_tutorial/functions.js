06d9f3a89b4a6500bb436aa53d0e92cde2c16a65
kitkatmia <alinka.markova@gmail.com>
2022-01-14 14:03:46 -0800

    Added syntax + function of spread and rest operators

--- mvhs.io_project/basics_of_javascript/free_code_camp_tutorial/functions.js
+++ mvhs.io_project/basics_of_javascript/free_code_camp_tutorial/functions.js
@@ -88,3 +88,30 @@ function subtract(num1 = 4, num2 = 2) {
 }
 console.log(subtract(7)); // num1 is set to 7 and num2 remains equal to 2
 
+// Rest operator - lets you taken in any number of arguments into the function
+const sum = (function () {
+    return function sum(...args) { // ... is the rest operator. It lets you take in any number of arguments (which are named args)
+        return args.reduce((a, b) => a + b, 0) // .reduce() method reduces the array to a single element by appling some function - the 0 at the end is the initial/default value
+    }
+})();
+console.log(sum());
+console.log(sum(1, 2));
+console.log(sum(5, 6, 7));
+
+// Spread operator - looks the same as the rest operator but expands an already existing array
+// PRE-SPREAD-OPERATOR
+const arr1 = ['JAN', 'FEB', 'MARCH', 'APRIL'];
+let arr2;
+(function () {
+    arr2 = arr1; // doesn't actually create a copy of arr1; it quite literally sets arr2 to arr1
+    arr1[0] = 'potato'
+})();
+console.log(arr2) // index 0 is potato because we set arr2 = arr1
+// AFTER-SPREAD-OPERATOR
+const arr3 = ['JAN', 'FEB', 'MARCH', 'APRIL'];
+let arr4;
+(function () {
+    arr4 = [...arr3]; // spreads the contents of arr3 and then puts them within a new array (making a copy)
+    arr3[0] = 'potato'
+})();
+console.log(arr4) // index 0 is still 'JAN'
